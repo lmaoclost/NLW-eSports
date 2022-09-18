22cccc1a9b887affeffe73710ef661ef61fe6b49
lmaoclost <renansmoliveira@gmail.com>
2022-09-18 16:46:04 -0300

    Subiu mobile aula 5

--- mobile/src/components/Background/index.tsx
+++ mobile/src/components/Background/index.tsx
@@ -1,17 +1,19 @@
 import { ImageBackground } from 'react-native';
+
 import backgroundImg from '../../assets/background-galaxy.png';
+
 import { styles } from './styles';
 
 interface Props {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export function Background({ children }: Props) {
   return (
     <ImageBackground
       source={backgroundImg}
-      style={styles.container}
       defaultSource={backgroundImg}
+      style={styles.container}
     >
       {children}
     </ImageBackground>
--- mobile/src/components/Heading/styles.ts
+++ mobile/src/components/Heading/styles.ts
@@ -1,5 +1,5 @@
-import { StyleSheet } from 'react-native';
-import { THEME } from '../../theme';
+import { StyleSheet } from "react-native";
+import { THEME } from "../../theme";
 
 export const styles = StyleSheet.create({
   container: {
@@ -9,11 +9,11 @@ export const styles = StyleSheet.create({
   title: {
     color: THEME.COLORS.TEXT,
     fontSize: THEME.FONT_SIZE.LG,
-    fontFamily: THEME.FONT_FAMILY.BLACK
+    fontFamily: THEME.FONT_FAMILY.BLACK,
   },
   subtitle: {
     color: THEME.COLORS.CAPTION_400,
     fontSize: THEME.FONT_SIZE.MD,
-    fontFamily: THEME.FONT_FAMILY.REGULAR
+    fontFamily: THEME.FONT_FAMILY.REGULAR,
   }
 });
